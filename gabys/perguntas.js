83cc89501c4b94a11452eb97000d2412cb494091
gabyzinha0 <144239790+gabyzinha0@users.noreply.github.com>
2024-09-24 09:39:53 -0300

    Add files via upload

---
+++ gabys/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Curso de ingles',
+    'Nossa plataforma oferece um curso de ingles gratuito, com diversos niveis, facies ate o dificil. Possuindo aulas online e off-line, quando você termina de fazer seu cadastro pode escolher entre duas opções aula online com professores ou apenas aulas off-line sem contato com professores.',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
